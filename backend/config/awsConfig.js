cef7a8ad072deaa86cd686de1f724bdf5c8bea3a
Lonewolf230 <manish2306j@gmail.com>
2025-09-06 12:17:27 +0530

    Added increased timeouts for cognito

--- backend/config/awsConfig.js
+++ backend/config/awsConfig.js
@@ -1,11 +1,17 @@
 const {CognitoIdentityProviderClient}=require("@aws-sdk/client-cognito-identity-provider")
 const {fromEnv}=require("@aws-sdk/credential-providers")
+const { NodeHttpHandler }=require("@aws-sdk/node-http-handler")
 require("dotenv").config()
 
 
 const client=new CognitoIdentityProviderClient({
     region:process.env.REGION,
-    credentials:fromEnv()
+    credentials:fromEnv(),
+    maxAttempts:3,
+    requestHandler: new NodeHttpHandler({
+        connectionTimeout:50000,
+        requestTimeout:50000
+    })
 })
 
 module.exports=client
