e83ff60152ee1a120413cab5cc4de049410cb854
Lonewolf230 <manish2306j@gmail.com>
2025-09-06 12:22:45 +0530

    Removed timeouts

--- backend/config/awsConfig.js
+++ backend/config/awsConfig.js
@@ -1,17 +1,12 @@
 const {CognitoIdentityProviderClient}=require("@aws-sdk/client-cognito-identity-provider")
 const {fromEnv}=require("@aws-sdk/credential-providers")
-const { NodeHttpHandler }=require("@aws-sdk/node-http-handler")
 require("dotenv").config()
 
 
 const client=new CognitoIdentityProviderClient({
     region:process.env.REGION,
     credentials:fromEnv(),
-    maxAttempts:3,
-    requestHandler: new NodeHttpHandler({
-        connectionTimeout:5000,
-        requestTimeout:8000
-    })
+    
 })
 
 module.exports=client
